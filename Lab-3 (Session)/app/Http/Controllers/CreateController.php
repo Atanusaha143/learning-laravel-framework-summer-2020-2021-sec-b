ba7ce628ac6ff96bcf3002f781432b6f8a00df6e
Atanusaha143 <atanu.saha415@gmail.com>
2021-06-10 21:19:33 +0600

    added password validation

--- Lab-3 (Session)/app/Http/Controllers/CreateController.php
+++ Lab-3 (Session)/app/Http/Controllers/CreateController.php
@@ -8,7 +8,7 @@ class CreateController extends Controller
 {
     public function index()
     {
-        return view('create.index');
+        return view('create.index')->with('msg','');
     }
 
     public function create(Request $req) 
@@ -16,33 +16,42 @@ class CreateController extends Controller
         $uname = $req->name;
         $email = $req->email;
         $pass = $req->password;
+        $cpass = $req->cpassword;
 
-        //Get file path
-        $file_path = storage_path().'/data.json';
+        if($pass == $cpass)
+        {
+            //Get file path
+            $file_path = storage_path().'/data.json';
 
-        //Load the file
-        $contents = file_get_contents($file_path);
+            //Load the file
+            $contents = file_get_contents($file_path);
 
-        //Decode the JSON data into a PHP array.
-        $contentsDecoded = json_decode($contents, true);
+            //Decode the JSON data into a PHP array.
+            $contentsDecoded = json_decode($contents, true);
 
-        //Get size of contentsDecoded
-        $len = sizeof($contentsDecoded['users']);
+            //Get size of contentsDecoded
+            $len = sizeof($contentsDecoded['users']);
 
-        //Create variables.
-        $contentsDecoded['users'][$len]['id']  = $len+1;
-        $contentsDecoded['users'][$len]['username'] = $uname;
-        $contentsDecoded['users'][$len]['password'] = $pass;
-        $contentsDecoded['users'][$len]['email'] = $email;
-        $contentsDecoded['users'][$len]['status'] = 1;
+            //Create variables.
+            $contentsDecoded['users'][$len]['id']  = $len+1;
+            $contentsDecoded['users'][$len]['username'] = $uname;
+            $contentsDecoded['users'][$len]['password'] = $pass;
+            $contentsDecoded['users'][$len]['email'] = $email;
+            $contentsDecoded['users'][$len]['status'] = 1;
 
-        //Encode the array back into a JSON string.
-        $json = json_encode($contentsDecoded);
+            //Encode the array back into a JSON string.
+            $json = json_encode($contentsDecoded);
 
-        //Save the file.
-        file_put_contents($file_path, $json);
+            //Save the file.
+            file_put_contents($file_path, $json);
 
-        //Redirect to user list
-        return redirect('/user/list');
+            //Redirect to user list
+            return redirect('/user/list');
+        }
+        else
+        {
+            $req->session()->flash('msg','**Password mismatch!');
+            return view('create.index')->with('msg', $req->session()->get('msg'));
+        }
     }
 }
